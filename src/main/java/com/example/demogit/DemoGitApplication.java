edc214552f130cf57dcea63669093c79671e6933
lls-fight <2303021618>
2023-03-15 09:24:41 +0800

    gitdemo01

---
+++ src/main/java/com/example/demogit/DemoGitApplication.java
@@ -0,0 +1,14 @@
+package com.example.demogit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGitApplication {
+
+    public static void main(String[] args) {
+        System.out.println("git-demo01");
+        SpringApplication.run(DemoGitApplication.class, args);
+    }
+
+}
